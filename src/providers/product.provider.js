e558576553ee164f462bd597331618853829d326
silverage <sep.non2@gmail.com>
2024-02-05 01:38:52 +0900

    :twisted_rightwards_arrows: [Merge] feature/49 병합

--- src/providers/product.provider.js
+++ src/providers/product.provider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getProductListResponseDTO, productCommonResponseDTO } from "../dtos/product.dto";
 import { getCategory, getProduct, getProductListToDB} from "../models/product.dao";
 
@@ -11,23 +10,4 @@ export const joinProductInfo = async (productId) => {
 export const joinProductList = async (query) => {
     const { paging = 3, cursorId = -1, keyword = "" } = query;
     return getProductListResponseDTO(await getProductListToDB(parseInt(cursorId), parseInt(paging), keyword));
-=======
-import { getProductInfoResponseDTO, getProductListResponseDTO } from "../dtos/product.dto";
-import { getProductDB, getCategoryDB } from "../models/product.dao";
-
-// 작품 정보 조회
-export const joinProductInfo = async () => {
-
-    return getProductInfoResponseDTO(await getProductDB());
-}
-
-// 작품 목록 조회
-export const joinProductList = async (categoryId, query) => {
-
-    const {paging = 3, cursorId = -1} = query;
-
-    console.log(categoryId, cursorId, paging);
-
-    return getProductListResponseDTO(await getCategoryDB(parseInt(categoryId), parseInt(cursorId), parseInt(paging)));
->>>>>>> origin/feature/49
 }
