36d461c49ab7554855fc7fbe234cb86ae9c418fd
silverage <sep.non2@gmail.com>
2024-02-05 15:31:02 +0900

    :hammer: [Update] Swagger update

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -11,8 +11,8 @@ authRouter.post('/login', asyncHandler(loginController));   // 로그인
 authRouter.post('/logout', jwtAuthenticationMiddleware, asyncHandler(logoutController)); // 로그아웃
 
 authRouter.post('/register', asyncHandler(registerController)); // 회원 가입
-authRouter.patch('/resign', asyncHandler(resignController));    // 회원 탈퇴
+authRouter.patch('/resign', jwtAuthenticationMiddleware, asyncHandler(resignController));    // 회원 탈퇴
 
-authRouter.post('/finduser/email', jwtAuthenticationMiddleware, asyncHandler(findEmailController));  // 이메일 찾기 (아이디 찾기)
-authRouter.post('/finduser/password', asyncHandler(changepasswordController)); // 비밀번호 변경
+authRouter.post('/finduser/email', jwtAuthenticationMiddleware, jwtAuthenticationMiddleware, asyncHandler(findEmailController));  // 이메일 찾기 (아이디 찾기)
+authRouter.post('/finduser/password', jwtAuthenticationMiddleware, asyncHandler(changepasswordController)); // 비밀번호 변경
 // authRouter.patch('/resign', resign);
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -4,18 +4,19 @@ import asyncHandler from 'express-async-handler';
 
 import { newProductController, editProductInfoController, getProductInfoController, getProductListController, dealProductController } from "../controllers/product.controller";
 import { imageUploader } from "../middleware/image.uploader.js";
+import { jwtAuthenticationMiddleware } from "../middleware/jwt.auth.js";
 
 export const productRouter = express.Router();
 
 // 작품 등록
-productRouter.post('/register', imageUploader.array('images', 10), asyncHandler(newProductController));
+productRouter.post('/register', jwtAuthenticationMiddleware, imageUploader.array('images', 10), asyncHandler(newProductController));
 
 // 작품 조회 관련(리스트, 세부 조회)
 productRouter.get('/list', asyncHandler(getProductListController)); // 작품 리스트 조회
 productRouter.get('/:productId', asyncHandler(getProductInfoController));   // 작품 정보 세부 조회
 
 // 거래 상태 변경
-productRouter.patch('/deal', asyncHandler(dealProductController));
+productRouter.patch('/deal', jwtAuthenticationMiddleware, asyncHandler(dealProductController));
 // 작품 정보 수정
-productRouter.patch('/:productId', imageUploader.array('images', 10), asyncHandler(editProductInfoController));
+productRouter.patch('/:productId', jwtAuthenticationMiddleware, imageUploader.array('images', 10), asyncHandler(editProductInfoController));
 
