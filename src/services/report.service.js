a2e6e1066594bb201e9692ea7c74bf221aaa5f7a
silverage <sep.non2@gmail.com>
2024-02-16 01:54:01 +0900

    :sparkles: [Feat] 신고 등록 시 메일 전송 기능 추가

--- src/services/report.service.js
+++ src/services/report.service.js
@@ -1,15 +1,22 @@
 import { registerReportResponseDTO } from "../dtos/report.dto"
+import { sendMailToBW } from "../middleware/mail";
 import { receiveReport } from "../models/report.dao";
 
 // 신고 접수
 export const joinReport = async (body) => {
-    
-    const joinReportData = await receiveReport({
+
+    const reportData = {
         'reporterId': body.reporterId,
         'reporteeId': body.reporteeId,
         'status': body.status,
         'context': body.context,
-    });
+    }
+    
+    console.log(reportData);
+
+    const joinReportData = await receiveReport(reportData);
+
+    sendMailToBW(reportData);   // 신고 메일 전송
 
     return registerReportResponseDTO(joinReportData);
     
