25a4318fdc067d5888f9f903ae46d4bfe07f22bf
silverage <sep.non2@gmail.com>
2024-02-19 21:13:57 +0900

    create

--- src/controllers/socket.controller.js
+++ src/controllers/socket.controller.js
@@ -5,6 +5,7 @@ export const connectRoom = async (info, socket, io) => {
     // 채팅방 접속을 위한 컨트롤러
     socket.join(info.roomId);
     console.log(info);
+    console.log(typeof(info.roomId));
     io.to(info.roomId).emit('connect-info', {result: await connectRoomService(info.roomId)})
 }
 
@@ -22,8 +23,8 @@ export const sendMessageController = async (message, socket, io) => {
 export const readMessageController = async (info, socket, io) => {
     // 메시지 읽었을 때 (채팅방 열람 시간 갱신)를 위한 컨트롤러
     
-    const { senderId } = info;
+    const { userId } = info;
     const result = await readMessageService(info);
 
-    if (result) io.to(senderId.toString()).emit('read-message', {result: result});
+    if (result) io.to(userId.toString()).emit('read-message', {result: result});
 }
