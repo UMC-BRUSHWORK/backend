220781299aefd31fde597aa6255849c6c2c7ff1a
silverage <sep.non2@gmail.com>
2024-07-24 00:47:19 +0900

    :sparkles: [Feat] 카카오 소셜로그인 구현

--- src/dtos/oauth.dto.js
+++ src/dtos/oauth.dto.js
@@ -1,13 +1,17 @@
 import moment from 'moment-timezone';
 
-export const kakaoLoginResponseDTO = (result, kakao_token) => {
-    console.log("time", result.access_at);
+export const kakaoLoginResponseDTO = (tokenInfo, userInfo) => {
     return {
         // userId, 닉네임
-        "userId": result.user_id,
-        "nickname": result.user_nickname,
-        "AccessTime": moment.utc(result.access_at).tz("Asia/Seoul").add(9, 'h').format('YYYY-MM-DD HH:mm:ss'),
-        "token": kakao_token
+        "userId": userInfo.user_id,
+        "nickname": userInfo.user_nickname,
+        "rate": userInfo.user_rate,
+        "profile": userInfo.user_profile,
+        "introduce": userInfo.user_introduce,
+        "status": userInfo.user_status,
+        "accessTime": moment.utc(userInfo.access_at).add(9, 'h').format('YYYY-MM-DD HH:mm:ss'),
+        "token": tokenInfo.access_token,
+        "tokenType": "bearer"
     };
 }
 
