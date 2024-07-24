aecb17f7d2b2670d3b1773577c61659b7ec843f6
silverage <sep.non2@gmail.com>
2024-07-24 22:10:34 +0900

    :sparkles: [Feat] 소셜 로그인 일부 수정

--- src/controllers/oauth.controller.js
+++ src/controllers/oauth.controller.js
@@ -7,7 +7,6 @@ dotenv.config();    // .env 파일 사용 (환경 변수 관리)
 
 // 카카오
 export const kakaoLoginController = async (req, res) => {
-  console.log("login");
   const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_CALLBACK_URL}&response_type=code&scope=account_email,profile_nickname`;
   res.redirect(kakaoLoginUrl);  // kakao로 redirect
 }
